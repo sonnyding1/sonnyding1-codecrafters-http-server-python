09185dd725d4f7f8f4274870e89ebf537a3266c0
Sonny Ding <sonnyding20@gmail.com>
2024-04-02 10:42:19 -0700

    pass stage 2

--- app/main.py
+++ app/main.py
@@ -6,7 +6,11 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
+    connection, client = server_socket.accept()  # wait for client
+
+    with connection:
+        connection.recv(1024)
+        connection.sendall("HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
